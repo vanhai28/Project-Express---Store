ed1aeabf4736f4ac4273b7063434949a415d94b8
Hai <vanhai.qtv@gmail.com>
2020-11-08 21:18:05 +0700

    modify home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+router.get('/login', function(req, res, next) {
+ res.render('login', { title: 'Login' });
+});
+router.get('/register', function(req, res, next) {
+ res.render('register', { title: 'Register' });
 });
 
 module.exports = router;
