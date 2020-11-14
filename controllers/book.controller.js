c6b22fc2f8391a0f6b02c79df6f318dbb142f7a6
Le Hao <o18ahl2811@gmail.com>
2020-11-14 11:49:15 +0700

    Book Detail

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -15,4 +15,45 @@ module.exports.bookShop = function(req, res, next){
         page:page 
       }
   )
+};
+
+module.exports.bookDetail = function(req, res, next){
+  const array = bookModel.listBook();
+
+   const f = array.find(x => x.ID_book == req.params.id );
+   res.render('./book/bookDetail', 
+   { title:"Detail", book_name: f.book_name, current_cost: f.current_cost});
 }
+
+// module.exports.bookDetail = async (req, res) => {
+//   var courses = await Courses.find().lean();
+//   coursesRender = courses.map(function (item) {
+//       item._id = item._id.toString();
+//       return item;
+//   })
+//   console.log(req.params.page);
+//   console.log(req.params.name);
+//   if (req.params.page ) {
+//       coursesRender = coursesRender.filter( x => x.categoryId === req.params.name);
+//       coursesRender = coursesRender.filter( x => x.topicId === req.params.page);
+      
+//   }
+//   if(req.query.topic)
+//   {
+//       if (req.query.sort === 'price_reduction') {
+//           coursesRender.sort(function (a, b) { return b.price - a.price });
+//       } else if (req.query.sort === 'price_increase') {
+  
+//           coursesRender.sort(function (a, b) { return a.price - b.price });
+//       } 
+//   }
+//   res.render('courses.hbs', {
+//       user: {
+//           sourse: "My Courses",
+//       },
+//       title: req.params.page,
+//       courses: coursesRender,
+//       sortCategory: req.params.name,
+//       sortTopic: req.params.page
+//   });
+// };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controllerDef = require('../controllers/controller.default');
-const bookController = require('../controllers/book.controller')
+const bookController = require('../controllers/book.controller');
+const bookModel = require('../model/bookModel');
+const { render } = require('../app');
 /* GET home page. */
 router.get('/',controllerDef.index);
 
@@ -9,6 +11,16 @@ router.get('/login',controllerDef.login);
 
 router.get('/register', controllerDef.register);
 
-router.get('/book-shop', bookController.bookShop)
+router.get('/book-shop', bookController.bookShop);
 
+router.get('/book-detail/:id', bookController.bookDetail);
+// router.get('/book-detail/:page', (req, res, next) => {
+//     //console.log(req.params.page);
+//    var array = bookModel.listBook();
+
+//    var f = array.find(x => x.ID_book == req.params.page );
+//    res.render('./book/bookDetail', 
+//    { title:"Book shop", book_name: f.book_name, current_cost: f.current_cost});
+    
+// });
 module.exports = router;
