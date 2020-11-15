03934667d8ecbe2d58654167513d2144738fa16e
Hai <vanhai.qtv@gmail.com>
2020-11-15 13:37:40 +0700

    update root folder

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,29 +1,31 @@
-const bookModel = require('../model/bookModel');
+const bookModel = require("../model/bookModel");
 
-module.exports.bookShop = function(req, res, next){
+module.exports.bookShop = function (req, res, next) {
   const page = parseInt(req.query.page) || 1;
   const pagination = bookModel.pagination(page);
   const prevPage = bookModel.prevPage(pagination[0].number);
   const nextPage = bookModel.nextPage(pagination[0].number);
 
-  res.render('./book/bookShop', 
-      { title:"Book shop", 
-        books :bookModel.getDisplayedBook(page),
-        pagination: pagination,
-        prevPage:prevPage,
-        nextPage: nextPage,
-        page:page 
-      }
-  )
+  res.render("pages/book/bookShop", {
+    title: "Book shop",
+    books: bookModel.getDisplayedBook(page),
+    pagination: pagination,
+    prevPage: prevPage,
+    nextPage: nextPage,
+    page: page,
+  });
 };
 
-module.exports.bookDetail = function(req, res, next){
+module.exports.bookDetail = function (req, res, next) {
   const array = bookModel.listBook();
 
-   const f = array.find(x => x.ID_book == req.params.id );
-   res.render('./book/bookDetail', 
-   { title:"Detail", book_name: f.book_name, current_cost: f.current_cost});
-}
+  const f = array.find((x) => x.ID_book == req.params.id);
+  res.render("pages/book/bookDetail", {
+    title: "Detail",
+    book_name: f.book_name,
+    current_cost: f.current_cost,
+  });
+};
 
 // module.exports.bookDetail = async (req, res) => {
 //   var courses = await Courses.find().lean();
@@ -36,16 +38,16 @@ module.exports.bookDetail = function(req, res, next){
 //   if (req.params.page ) {
 //       coursesRender = coursesRender.filter( x => x.categoryId === req.params.name);
 //       coursesRender = coursesRender.filter( x => x.topicId === req.params.page);
-      
+
 //   }
 //   if(req.query.topic)
 //   {
 //       if (req.query.sort === 'price_reduction') {
 //           coursesRender.sort(function (a, b) { return b.price - a.price });
 //       } else if (req.query.sort === 'price_increase') {
-  
+
 //           coursesRender.sort(function (a, b) { return a.price - b.price });
-//       } 
+//       }
 //   }
 //   res.render('courses.hbs', {
 //       user: {
--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,25 @@ router.get("/", controllerDef.index);
 
 router.get("/login", controllerDef.login);
 
+router.get("/about-us", controllerDef.aboutUs);
+
 router.get("/register", controllerDef.register);
+
+router.get("/cart", controllerDef.cart);
+
+router.get("/checkout", controllerDef.checkout);
+
+router.get("/contact", controllerDef.contact);
+
+router.get("/faq", controllerDef.faq);
 
 router.get("/book-shop", bookController.bookShop);
 
 router.get("/book-detail/:id", bookController.bookDetail);
+
+router.post("/auth/login", authController.authLoginUser);
+
+module.exports = router;
 // router.get('/book-detail/:page', (req, res, next) => {
 //     //console.log(req.params.page);
 //    var array = bookModel.listBook();
@@ -23,7 +37,3 @@ router.get("/book-detail/:id", bookController.bookDetail);
 //    { title:"Book shop", book_name: f.book_name, current_cost: f.current_cost});
 
 // });
-
-router.post("/auth/login", authController.authLoginUser);
-
-module.exports = router;
