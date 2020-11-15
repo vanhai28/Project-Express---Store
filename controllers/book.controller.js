9237526fc00226baa4a1a01b0fa9cbcbd4f2896b
Hai <vanhai.qtv@gmail.com>
2020-11-15 09:44:45 +0700

    update login

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -3,8 +3,8 @@ const bookModel = require('../model/bookModel');
 module.exports.bookShop = function(req, res, next){
   const page = parseInt(req.query.page) || 1;
   const pagination = bookModel.pagination(page);
-   const prevPage = bookModel.prevPage(pagination[0].number);
-   const nextPage = bookModel.nextPage(pagination[0].number);
+  const prevPage = bookModel.prevPage(pagination[0].number);
+  const nextPage = bookModel.nextPage(pagination[0].number);
 
   res.render('./book/bookShop', 
       { title:"Book shop", 
